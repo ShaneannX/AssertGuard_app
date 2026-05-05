16d58cd7b1a9b810a06c2c539319fc14d33cbc0a
ShaneannX <shaneann.ahtack@gmail.com>
2026-05-05 18:20:32 +0100

    added to get all items instead of just only based on Job Id. Also added to delete all items in local database needing for full correction so sync can work

--- lib/data/local_database/data_access_objects/inspection_items_dao.dart
+++ lib/data/local_database/data_access_objects/inspection_items_dao.dart
@@ -13,37 +13,45 @@ class InspectionItemsDao extends DatabaseAccessor<AppDatabase>
   Future<int> insertItem(InspectionItemsCompanion item) =>
       into(inspectionItems).insert(item);
 
-  // READ all inspection items for the job due to FK constraint. 
+  // Gets all InspectionItems
+  Future<List<InspectionItem>> getAllItems() {
+    return (select(
+      inspectionItems,
+    )..where((tbl) => tbl.isDeleted.equals(false))).get();
+  }
+
+  // READ all inspection items for the job due to FK constraint.
   Future<List<InspectionItem>> getItemsForJob(String jobId) {
-    return (select(inspectionItems)
-          ..where((tbl) => tbl.jobId.equals(jobId))
-          ..where((tbl) => tbl.isDeleted.equals(false)))
-        .get();
+    print('selecting items with $jobId');
+    print(select(inspectionItems).get());
+    return (select(
+      inspectionItems,
+    )..where((tbl) => tbl.jobId.equals(jobId))).get();
   }
 
   // READ single item
   Future<InspectionItem?> getItemById(String id) {
-    return (select(inspectionItems)
-          ..where((tbl) => tbl.id.equals(id)))
-        .getSingleOrNull();
+    return (select(
+      inspectionItems,
+    )..where((tbl) => tbl.id.equals(id))).getSingleOrNull();
   }
 
-  // READ pending items (for sync) in sync service. 
+  // READ pending items (for sync) in sync service.
   Future<List<InspectionItem>> getPendingItems() {
-    return (select(inspectionItems)
-          ..where((tbl) => tbl.syncStatus.equals('pending')))
-        .get();
+    return (select(
+      inspectionItems,
+    )..where((tbl) => tbl.syncStatus.equals('pending'))).get();
   }
 
   // UPDATE item.
   Future<bool> updateItem(String id, InspectionItemsCompanion fields) async {
-    final rows = await (update(inspectionItems)
-          ..where((tbl) => tbl.id.equals(id)))
-        .write(fields);
+    final rows = await (update(
+      inspectionItems,
+    )..where((tbl) => tbl.id.equals(id))).write(fields);
     return rows > 0;
   }
 
-  // SOFT DELETE - Marking the deleted flag as true. 
+  // SOFT DELETE - Marking the deleted flag as true.
   Future<void> softDelete(String id) {
     return (update(inspectionItems)..where((tbl) => tbl.id.equals(id))).write(
       const InspectionItemsCompanion(
@@ -58,11 +66,17 @@ class InspectionItemsDao extends DatabaseAccessor<AppDatabase>
     return (delete(inspectionItems)..where((tbl) => tbl.id.equals(id))).go();
   }
 
+  // Hard delete items that has an empty jobId - as they will not be able to show and mess up the sync process.
+  Future<void> deleteItemsWithEmptyJobId() async {
+    print('DELETING EMPTY ITEMS');
+    await delete(inspectionItems).go();
+  }
+
   // UPDATE (Sync)
   Future<int> updateItemSync(String id, InspectionItemsCompanion fields) {
-    return (update(inspectionItems)
-          ..where((tbl) => tbl.id.equals(id)))
-        .write(fields);
+    return (update(
+      inspectionItems,
+    )..where((tbl) => tbl.id.equals(id))).write(fields);
   }
 
   // MARK AS SYNCED
@@ -74,40 +88,46 @@ class InspectionItemsDao extends DatabaseAccessor<AppDatabase>
 
   // INSERT OR UPDATE FROM CLOUD
   Future<void> insertFromCloud(Map<String, dynamic> json) async {
-    final id = json['id'].toString();
+  final id = json['id'].toString();
 
+  try {
     final updated = await (update(inspectionItems)
           ..where((tbl) => tbl.id.equals(id)))
         .write(
-          InspectionItemsCompanion(
-            jobId: Value(json['job_id'].toString()),
-            description: Value(json['description'] ?? ''),
-            notes: Value(json['notes']),
-            createdBy: Value(json['created_by'] ?? 'Unknown'),
-            updatedAt: json['updated_at'] != null
-                ? Value(DateTime.parse(json['updated_at']))
-                : const Value(null),
-            syncStatus: const Value('synced'),
-            isDeleted: Value(json['is_deleted'] ?? false),
-          ),
-        );
+      InspectionItemsCompanion(
+        id: Value(id),
+        jobId: Value(json['jobId'].toString()),
+        description: Value(json['description'] ?? ''),
+        notes: Value(json['notes'] ?? ''),                    
+        createdBy: Value(json['createdBy'] ?? 'Unknown'),      
+        updatedAt: json['updatedAt'] != null
+            ? Value(DateTime.parse(json['updatedAt']))
+            : const Value(null),                               
+        syncStatus: Value('synced'),     
+        isDeleted: const Value(false),
+      ),
+    );
 
     if (updated == 0) {
       await into(inspectionItems).insert(
         InspectionItemsCompanion.insert(
           id: id,
-          jobId: json['job_id'].toString(),
+          jobId: json['jobId'].toString(),
           description: json['description'] ?? '',
-          notes: Value(json['notes']),
-          createdBy: json['created_by'] ?? 'Unknown',
-          createdAt: DateTime.parse(json['created_at']),
-          updatedAt: json['updated_at'] != null
-              ? Value(DateTime.parse(json['updated_at']))
+          notes: Value(json['notes'] ?? ''),                  
+          createdBy: json['createdBy'] ?? 'Unknown',   
+          createdAt: DateTime.parse(json['createdAt']),
+          updatedAt: json['updatedAt'] != null
+              ? Value(DateTime.parse(json['updatedAt']))
               : const Value(null),
-          syncStatus: 'synced',
-          isDeleted: Value(json['is_deleted'] ?? false),
+          syncStatus: 'synced', 
+          isDeleted: const Value(false),
         ),
       );
     }
+  } catch (e) {
+    print('Failed syncing Items from cloud db to local db: $e');
   }
+}
+
 }
