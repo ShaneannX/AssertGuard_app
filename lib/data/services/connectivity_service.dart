498dc8a2a196a39bd9d795159663545ec9959d39
ShaneannX <shaneann.ahtack@gmail.com>
2026-05-05 18:23:03 +0100

    Connection wasn't updating in the UI so added stream and status tracker so can trace when user is online

--- lib/data/services/connectivity_service.dart
+++ lib/data/services/connectivity_service.dart
@@ -1,9 +1,30 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'dart:async';
 
 class ConnectivityService {
-  Future<bool> isOnline() async {
-    final results = await Connectivity().checkConnectivity();
-    return results.any(
+  final _controller =
+      StreamController<bool>.broadcast(); // added stream so UI can update.
+
+  ConnectivityService() {
+    Connectivity().onConnectivityChanged.listen((status) {
+      final online = status.any(
+        (r) => [
+          ConnectivityResult.wifi,
+          ConnectivityResult.mobile,
+          ConnectivityResult.ethernet,
+          ConnectivityResult.vpn,
+        ].contains(r),
+      );
+
+      _controller.add(online);
+    });
+  }
+
+  Stream<bool> get onStatusChange => _controller.stream;
+
+  Future<bool> get isOnline async {
+    final status = await Connectivity().checkConnectivity();
+    return status.any(
       (r) => [
         ConnectivityResult.wifi,
         ConnectivityResult.mobile,
