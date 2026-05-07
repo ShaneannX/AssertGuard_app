ac5d23b65aa626f4938924ac4b29d7d629ddbba8
ShaneannX <shaneann.ahtack@gmail.com>
2026-05-07 14:32:52 +0100

    Converted into a singleton with .instance. onStatusChange stream and caching inital value with currentStatus

--- lib/data/services/connectivity_service.dart
+++ lib/data/services/connectivity_service.dart
@@ -2,20 +2,38 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'dart:async';
 
 class ConnectivityService {
-  final _controller =
-      StreamController<bool>.broadcast(); // added stream so UI can update.
+  static final instance = ConnectivityService._internal();
+  ConnectivityService._internal() {
+    _init();
+  }
 
-  ConnectivityService() {
-    Connectivity().onConnectivityChanged.listen((status) {
-      final online = status.any(
-        (r) => [
-          ConnectivityResult.wifi,
-          ConnectivityResult.mobile,
-          ConnectivityResult.ethernet,
-          ConnectivityResult.vpn,
-        ].contains(r),
-      );
+  final _controller = StreamController<bool>.broadcast();
 
+  bool _currentStatus = false;
+  bool get currentStatus => _currentStatus;
+
+  void _init() {
+    // LIST-BASED API
+    Connectivity().onConnectivityChanged.listen((results) {
+      final online = results.any((r) =>
+          r == ConnectivityResult.wifi ||
+          r == ConnectivityResult.mobile ||
+          r == ConnectivityResult.ethernet ||
+          r == ConnectivityResult.vpn);
+
+      _currentStatus = online;
+      _controller.add(online);
+    });
+
+    // Initial status
+    Connectivity().checkConnectivity().then((results) {
+      final online = results.any((r) =>
+          r == ConnectivityResult.wifi ||
+          r == ConnectivityResult.mobile ||
+          r == ConnectivityResult.ethernet ||
+          r == ConnectivityResult.vpn);
+
+      _currentStatus = online;
       _controller.add(online);
     });
   }
@@ -23,14 +41,11 @@ class ConnectivityService {
   Stream<bool> get onStatusChange => _controller.stream;
 
   Future<bool> isOnline() async {
-    final status = await Connectivity().checkConnectivity();
-    return status.any(
-      (r) => [
-        ConnectivityResult.wifi,
-        ConnectivityResult.mobile,
-        ConnectivityResult.ethernet,
-        ConnectivityResult.vpn,
-      ].contains(r),
-    );
+    final results = await Connectivity().checkConnectivity();
+    return results.any((r) =>
+        r == ConnectivityResult.wifi ||
+        r == ConnectivityResult.mobile ||
+        r == ConnectivityResult.ethernet ||
+        r == ConnectivityResult.vpn);
   }
 }
